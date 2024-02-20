fc379cdd2c44295fb80d99efec7b57af86aabc28
HuyNVN <huynvn97@gmail.com>
2024-02-20 19:57:49 +0700

    style(TextInput): update textinput border

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -11,18 +11,15 @@ export type TextInputProps = RNTextInputProps & {};
 export default function TextInput(props: TextInputProps): React.ReactElement {
   const {style, ...rest} = props;
 
-  return (
-    <View style={[styles.container]}>
-      <RNTextInput style={[styles.input, style]} {...rest} />
-    </View>
-  );
+  return <RNTextInput style={[styles.input, style]} {...rest} />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingVertical: 5,
-  },
   input: {
     minHeight: 40,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'gray',
+    paddingHorizontal: 10,
   },
 });
